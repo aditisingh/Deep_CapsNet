5fc04284a93028fa3873e55c14053b259f115bd3
Aditi Singh <787aditi@gmail.com>
2019-01-23 14:14:16 -0600

    Colorbar added

--- plot_clusters.py
+++ plot_clusters.py
@@ -10,7 +10,7 @@ Y=[f[44] for f in F]
 X1=X.reshape(-1,16*2)
 
 X_embedded = TSNE(n_components=2).fit_transform(X1)
-kmeans = KMeans(n_clusters=2, random_state=0).fit_predict(X_embedded)
+kmeans = KMeans(n_clusters=20, random_state=0).fit_predict(X_embedded)
 
 #color is same for same class type: gnd truth, number allocated in the cluster number
 plt.scatter(X_embedded[:,0],X_embedded[:,1],c=Y)
@@ -18,7 +18,7 @@ for i, y in enumerate(Y):
      plt.annotate(kmeans[i],(X_embedded[i,0],X_embedded[i,1]))
 
 
-
+plt.colorbar(label='Actual Label')
 plt.show()
 
 
