0297d9315f9bf2d47fddd2a9618dcfbeb9b32eac
Aditi Singh <787aditi@gmail.com>
2019-01-23 14:03:01 -0600

    Update plot_clusters.py

--- plot_clusters.py
+++ plot_clusters.py
@@ -12,6 +12,7 @@ X1=X.reshape(-1,16*2)
 X_embedded = TSNE(n_components=2).fit_transform(X1)
 kmeans = KMeans(n_clusters=2, random_state=0).fit_predict(X_embedded)
 
+#color is same for same class type: gnd truth, number allocated in the cluster number
 plt.scatter(X_embedded[:,0],X_embedded[:,1],c=Y)
 for i, y in enumerate(Y):
      plt.annotate(kmeans[i],(X_embedded[i,0],X_embedded[i,1]))
